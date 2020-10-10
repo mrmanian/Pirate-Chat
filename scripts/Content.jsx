3cf3e0d5f3b29816ed88fb5c4817ecf505f749bf
mrmanian <mikemanian@gmail.com>
2020-10-10 21:16:21 +0000

    Add css to chatbox content

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
+import { Users } from './Users';
 import { Button } from './Button';
 import { Socket } from './Socket';
+
+import './Content.css';
 
 export function Content() {
     const [messages, setMessages] = useState([]);
     
     function getNewMessages() {
         useEffect(() => {
-            Socket.on('messages received', updateMessages)
+            Socket.on('messages received', updateMessages);
             return () => {
                 Socket.off('messages received', updateMessages);
-            }
+            };
         });
     }
     
@@ -24,12 +27,17 @@ export function Content() {
     
     return (
         <div>
-            <h1>Messenger</h1>
-            <p style={{"border-style": "inset", "width": "40%", "margin": "auto", "padding-bottom": "15px", "margin-bottom": "25px"}}>
-                {messages.map((message, index) => (
-                <li style={{"list-style-type": "none"}} key={index}>{message}</li>))}
-            </p>
-            <Button />
+            <h1>Chat App</h1>
+            <div className='margin'><Users /></div>
+            <div className='outerContainer'>
+                <div className='container'>
+                    <ul>
+                        {messages.map((message, index) => (
+                        <li className='list' key={index}>{message}</li>))}
+                    </ul>
+                </div>
+                <div className='fixed'><Button /></div>
+            </div>
         </div>
     );
 }
