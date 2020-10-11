d80cf08826f76efa881504892a6d99cb4fa7a66b
mrmanian <mikemanian@gmail.com>
2020-10-11 22:06:25 +0000

    Change size limit of character to the max

--- models.py
+++ models.py
@@ -1,9 +1,10 @@
 import flask_sqlalchemy
 from app import db
 
+# PSQL/SQLAlchemy database model
 class Chat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    message = db.Column(db.String(120))
+    message = db.Column(db.String(10485760))
     
     def __init__(self, a):
         self.message = a
