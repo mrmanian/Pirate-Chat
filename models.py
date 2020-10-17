ca0a0d0167ffb7dae3b2ed33e84d01dd76660f6f
mrmanian <mikemanian@gmail.com>
2020-10-17 05:18:24 +0000

    Add new database columns to store username, picurl, and message

--- models.py
+++ models.py
@@ -2,12 +2,16 @@ import flask_sqlalchemy
 from app import db
 
 # PSQL/SQLAlchemy database model
-class Chat(db.Model):
+class ChatHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String(120))
+    picUrl = db.Column(db.String(600))
     message = db.Column(db.String(10485760))
     
-    def __init__(self, a):
-        self.message = a
+    def __init__(self, userName, picUrl, message):
+        self.userName = userName
+        self.picUrl = picUrl
+        self.message = message
         
     def __repr__(self):
-        return '<Chat message: %s>' % self.message 
+        return '<userName: {}\npicUrl: {}\nmessage: {}'.format(self.userName, self.picUrl, self.message)
