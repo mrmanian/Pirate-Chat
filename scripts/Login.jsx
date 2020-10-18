b3671853128fe737899be18af69ace1927ffa8d4
mrmanian <mikemanian@gmail.com>
2020-10-18 22:17:59 +0000

    Create login page and center content in container

---
+++ scripts/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Login.css';
+
+export function Login() {
+    return (
+        <div className='loginContainer'>
+            <h1>Login to Enter</h1>
+        </div>
+    )
+}
