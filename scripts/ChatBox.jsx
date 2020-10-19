aa63e4efd5d88452439abef1ee82dd9a0a69cb32
mrmanian <mikemanian@gmail.com>
2020-10-19 04:14:03 +0000

    Put message chat box code in separate file

---
+++ scripts/ChatBox.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import Linkify from 'react-linkify';
+
+import { Socket } from './Socket';
+
+import './ChatBox.css';
+
+export function ChatBox() {
+    const [userNames, setUserNames] = useState([]);
+    const [picUrls, setPicUrls] = useState([]);
+    const [messages, setMessages] = useState([]);
+    
+// Display links on new page
+    const componentDecorator = (href, text, key) => (
+       <a href={href} key={key} target='_blank' rel='noopener noreferrer'>
+         {text}
+       </a>
+    );
+
+// Gets message via socket
+    useEffect(() => {
+        Socket.on('messages received', updateData);
+        return () => {
+            Socket.off('messages received', updateData);
+        };
+    }, []);
+    
+// Update hook with the new data
+    function updateData(data) {
+        console.log('Received usernames from the server: ' + data['allUserNames']);
+        console.log('Received picurls from the server: ' + data['allPicUrls']);
+        console.log('Received messages from the server: ' + data['allMessages']);
+        setUserNames(data['allUserNames']);
+        setPicUrls(data['allPicUrls']);
+        setMessages(data['allMessages']);
+    }
+    
+// Check if message is an image
+    function checkIsImg(data) {
+        console.log(data);
+        if (data.indexOf('.jpg') !== -1) {
+            return (<img className='images' src={data} />);
+        }
+        else if (data.indexOf('.png') !== -1) {
+            return (<img className='images' src={data} />);
+        }
+        else if (data.indexOf('.gif') !== -1) {
+            return (<img className='images' src={data} />);
+        }
+        else {
+            return data;
+        }
+    }
+    
+    return (
+        <Linkify componentDecorator={componentDecorator}>
+            <ul>
+                {messages.map((message, index) => (
+                <li className='list' key={index}><img className='profilePic' src={picUrls[index]} /> {userNames[index]}: {checkIsImg(message)}</li>))}
+            </ul>
+        </Linkify>
+    );
+}
--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,61 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import Linkify from 'react-linkify';
+import React from 'react';
 
 import { Users } from './Users';
 import { MessageForm } from './MessageForm';
-import { Socket } from './Socket';
 import { Login } from './Login';
+import { ChatBox } from './ChatBox';
 
 import './Content.css';
 
+// Displays page contents
 export function Content() {
-    const [userNames, setUserNames] = useState([]);
-    const [picUrls, setPicUrls] = useState([]);
-    const [messages, setMessages] = useState([]);
-    
-// Display links on new page
-    const componentDecorator = (href, text, key) => (
-       <a href={href} key={key} target='_blank' rel='noopener noreferrer'>
-         {text}
-       </a>
-    );
-
-// Gets message via socket
-    useEffect(() => {
-        Socket.on('messages received', updateData);
-        return () => {
-            Socket.off('messages received', updateData);
-        };
-    });
-    
-// Update hook with the new data
-    function updateData(data) {
-        console.log('Received usernames from the server: ' + data['allUserNames']);
-        console.log('Received picurls from the server: ' + data['allPicUrls']);
-        console.log('Received messages from the server: ' + data['allMessages']);
-        setUserNames(data['allUserNames']);
-        setPicUrls(data['allPicUrls']);
-        setMessages(data['allMessages']);
-    }
-    
-// Check if message is an image
-    function checkIsImg(data) {
-        console.log(data);
-        if (data.indexOf('.jpg') !== -1) {
-            return (<img src={data} />);
-        }
-        else if (data.indexOf('.png') !== -1) {
-            return (<img src={data} />);
-        }
-        else if (data.indexOf('.gif') !== -1) {
-            return (<img src={data} />);
-        }
-        else {
-            return data;
-        }
-    }
-    
-// Creates chat box and message content
     return (
         <div>
             <Login />
@@ -64,12 +17,7 @@ export function Content() {
                     <Users />
                 </div>
                 <div className='container'>
-                    <Linkify componentDecorator={componentDecorator}>
-                        <ul>
-                            {messages.map((message, index) => (
-                            <li className='list' key={index}><img src={picUrls[index]} /> {userNames[index]}: {checkIsImg(message)}</li>))}
-                        </ul>
-                    </Linkify>
+                    <ChatBox />
                 </div>
                 <MessageForm />
             </div>
