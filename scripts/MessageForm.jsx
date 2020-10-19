ea37324ca90534f0d2e26076cf689d2061e3144f
mrmanian <mikemanian@gmail.com>
2020-10-19 04:04:28 +0000

    Disable input and button if user is not authenticated

--- scripts/MessageForm.jsx
+++ scripts/MessageForm.jsx
@@ -7,16 +7,18 @@ import './MessageForm.css';
 export function MessageForm() {
     const [userName, setUserName] = useState('');
     const [picUrl, setPicUrl] = useState('');
+    const [state, setState] = useState(true);
 
 // Set username for each connection
     useEffect(() => {
         Socket.on('userName', (data) => {
             setUserName(data['userName']);
             setPicUrl(data['picUrl']);
+            setState(false);
             console.log('Received username: ' + data['userName']);
-            console.log('Received profile pic url: ' + data['picUrl']);
+            console.log('Received profile picture url: ' + data['picUrl']);
         });
-    });
+    }, []);
     
 // Handles form submit- get message value and emit it via socket
     function handleSubmit(event) {
@@ -40,10 +42,10 @@ export function MessageForm() {
     return (
         <form className='formContainer' onSubmit={handleSubmit} autoComplete='off'>
             <div className="inputContainer">
-                <input type='text' id='text' placeholder='Enter yer message...'></input>
+                <input type='text' id='text' placeholder='Enter yer message...' disabled={state}></input>
             </div>
             <div className="buttonContainer">
-                <button type='submit'>Send<br/>Message</button>
+                <button type='submit' disabled={state}>Send<br/>Message</button>
             </div>
         </form>
     );
