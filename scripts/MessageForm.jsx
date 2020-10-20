ba75e8b73375a1e369be4d97c5fdaaac43b688f7
mrmanian <mikemanian@gmail.com>
2020-10-20 17:15:33 +0000

    Add feature to show when current user is typing

--- scripts/MessageForm.jsx
+++ scripts/MessageForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import debounce from 'lodash/debounce';
 
 import { Socket } from './Socket';
 
@@ -8,8 +9,22 @@ export function MessageForm() {
     const [userName, setUserName] = useState('');
     const [picUrl, setPicUrl] = useState('');
     const [state, setState] = useState(true);
+    const [isTyping, setIsTyping] = useState(false);
+    
+// Shows when the current user is typing
+    const handleTyping = debounce(function() {
+        setIsTyping(false);
+    }, 1500);
 
-// Set username for each connection
+    useEffect(() => {
+        handleTyping();
+    }, [isTyping]);
+  
+    function handleChange() {
+        setIsTyping(true);
+    }
+    
+// Set username and profile picture for each connection
     useEffect(() => {
         Socket.on('userName', (data) => {
             setUserName(data['userName']);
@@ -19,7 +34,7 @@ export function MessageForm() {
             console.log('Received profile picture url: ' + data['picUrl']);
         });
     }, []);
-    
+  
 // Handles form submit- get message value and emit it via socket
     function handleSubmit(event) {
         let newMessage = document.getElementById('text').value;
@@ -38,15 +53,32 @@ export function MessageForm() {
         event.preventDefault();
     }
 
-// Creates input box and button
+// Handle logout button
+    function handleLogout(event) {
+        Socket.emit('logout');
+        setState(true);
+        event.preventDefault();
+    }
+    
+    var name = JSON.stringify(userName).replace(/\"/g, "");
+    
+    close();
     return (
-        <form className='formContainer' onSubmit={handleSubmit} autoComplete='off'>
-            <div className="inputContainer">
-                <input type='text' id='text' placeholder='Enter yer message...' disabled={state}></input>
+        <div>
+            <div className='outer'>
+                <p>{isTyping && name + ' is typing...'}</p>
             </div>
-            <div className="buttonContainer">
-                <button type='submit' disabled={state}>Send<br/>Message</button>
-            </div>
-        </form>
+            <form className='formContainer' onSubmit={handleSubmit} autoComplete='off'>
+                <div className='inputContainer'>
+                    <input type='text' id='text' placeholder='Enter yer message...' disabled={state} onChange={handleChange}></input>
+                </div>
+                <div className='buttonContainer'>
+                    <button type='submit' disabled={state}>Send<br/>Message</button>
+                </div>
+                <div className='buttonContainer'>
+                    <button className='logout' onClick={handleLogout} disabled={state}>Logout</button>
+                </div>
+            </form>
+        </div>
     );
 }
